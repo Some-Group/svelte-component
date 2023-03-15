aba02fb640c1c19c4419bde0d54698fd1dc48b35
Boiao Ch <doubiliao@gmail.com>
2023-03-15 21:49:56 +0800

    chore: use sveltejs/kit package build npm package

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,51 +1,54 @@
 /// <reference types="rollup" />
 
-import svelte from 'rollup-plugin-svelte'
-import autoPreprocess from 'svelte-preprocess'
-import resolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
-import * as fs from 'fs'
+import svelte from "rollup-plugin-svelte";
+import autoPreprocess from "svelte-preprocess";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import tsconfigPaths from "rollup-plugin-tsconfig-paths";
+import * as fs from "fs";
 
-const compoentPath = 'src/components/'
-const result = fs.readdirSync(compoentPath)
+const compoentPath = "src/components/";
+const result = fs.readdirSync(compoentPath);
 
-const componentsNames = []
+const componentsNames = [];
 
-result.forEach(name => {
-    if (fs.lstatSync(compoentPath + name).isDirectory() && fs.lstatSync(compoentPath + name + '/index.ts').isFile()) {
-        componentsNames.push(name)
-    }
-})
+result.forEach((name) => {
+  if (
+    fs.lstatSync(compoentPath + name).isDirectory() &&
+    fs.lstatSync(compoentPath + name + "/index.ts").isFile()
+  ) {
+    componentsNames.push(name);
+  }
+});
 
 function buildConfig(componentName, outputName) {
-    return {
-        input: `src/components/${componentName}/index.ts`,
-        output: {
-            format: 'esm',
-            file: `dist/es/${outputName}/index.js`
-        },
-        sourcemap: true,
-        plugins: [
-            svelte({
-                preprocess: autoPreprocess({
-                    sourceMap: true,
-                }),
-            }),
-            resolve({
-                browser: true,
-                dedupe: ['svelte'],
-                exportConditions: ['svelte'],
-                extensions: ['.svelte']
-            }),
-            typescript({ sourceMap: true }),
-            // terser()
-        ],
-        external: ['classnames', 'svelte', 'svelte/internal']
-    }
-
+  return {
+    input: `src/components/${componentName}/index.ts`,
+    output: {
+      format: "esm",
+      file: `dist/es/${outputName}/index.js`,
+    },
+    sourcemap: true,
+    plugins: [
+      tsconfigPaths(),
+      svelte({
+        preprocess: autoPreprocess({
+          sourceMap: true,
+        }),
+      }),
+      resolve({
+        browser: true,
+        dedupe: ["svelte"],
+        exportConditions: ["svelte"],
+        extensions: [".svelte"],
+      }),
+      typescript({ sourceMap: true }),
+      // terser()
+    ],
+    external: ["classnames", "svelte", "svelte/internal"],
+  };
 }
 
-
-export default componentsNames.map(name => {
-    return buildConfig(name, name)
-})
+export default componentsNames.map((name) => {
+  return buildConfig(name, name);
+});
